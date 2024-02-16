ae0244ae3f168a2913e20bd262056679b5977676
hyungji <icthelight@naver.com>
2024-02-17 01:25:49 +0900

    ✨ feat: QueryClientProvider

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,13 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import ErrorBoundary from "./common/components/error/ErrorBoundary";
 import Posts from "./pages/Posts";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <ErrorBoundary>
-      <Routes>
-        <Route path="/posts" element={<Posts />} />
-      </Routes>
-    </ErrorBoundary>
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/posts" element={<Posts />} />
+        </Routes>
+      </ErrorBoundary>
+    </QueryClientProvider>
   );
 }
 
